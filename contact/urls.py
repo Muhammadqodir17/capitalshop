4cbf2991e5a01c0aff23ff040dbbafc0746a8258
Muhammadqodir <km1728.uz@gmail.com>
2024-02-20 14:41:43 +0500

    category

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from .views import contact_view
+
 
 urlpatterns = [
-
+    path('', contact_view)
 ]
--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from config.base_models import BaseModel
 
-# Create your models here.
+
+class ProductCategory(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseModel):
+    price = models.FloatField()
+    discount = models.IntegerField(default=0)
+    price_with_discount = models.FloatField(default=0)
+
+    def calculate_discount(self):
+        pass
+
+
+
+
