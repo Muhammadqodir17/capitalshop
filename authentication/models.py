dde0ddd43bdf0ca2069949fe2b53f3b075f44dbf
Muhammadqodir <km1728.uz@gmail.com>
2024-12-11 19:11:39 +0500

    changed

---
+++ authentication/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .utils import phone_validation
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=13, validators=[phone_validation], unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'{self.username}'
+
+
+class CustomCard(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    pan = models.IntegerField(default=0)
+    cvv = models.CharField(max_length=3)
+    bank_name = models.CharField(max_length=100)
+    card_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13, validators=[phone_validation])
+    balance = models.PositiveIntegerField(default=0)
+    created_month = models.IntegerField(default=0)
+    created_year = models.IntegerField(default=0)
+    expired_month = models.IntegerField(default=0)
+    expired_year = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class UserCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    pan = models.IntegerField(default=0)
+    cvv = models.CharField(max_length=3)
+    bank_name = models.CharField(max_length=100)
+    card_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13, validators=[phone_validation])
+    balance = models.PositiveIntegerField(default=0)
+    created_month = models.IntegerField(default=0)
+    created_year = models.IntegerField(default=0)
+    expired_month = models.IntegerField(default=0)
+    expired_year = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
